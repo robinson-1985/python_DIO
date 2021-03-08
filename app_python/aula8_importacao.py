76f08ab8a557714f0c95d733e8ef6c4257c5a984
Robinson <https://github.com/robinson-1985>
2021-03-08 16:09:37 -0300

    Aprendendo sobre python

--- app_python/aula8_importacao.py
+++ app_python/aula8_importacao.py
@@ -8,7 +8,7 @@
 
 from aula7_televisao import Televisao
 from aula7_calculadora1 import Calculadora
-from aula8_contador_letras import contador_letras
+from aula8_contador_palavras import contador_letras
 
 if __name__ == '__main__':
     televisao = Televisao()
