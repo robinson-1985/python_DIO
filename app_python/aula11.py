7f1066f0778b941fd0343fed96f91542a8773d6a
Robinson <https://github.com/robinson-1985>
2021-03-10 19:40:24 -0300

    Aprendendo sobre Python

--- app_python/aula11.py
+++ app_python/aula11.py
@@ -6,13 +6,25 @@ Criação de exceções customizadas. '''
 
 
 lista = [1, 10]
+
 try :
+    arquivo= open('teste.txt', 'r')
+    texto = arquivo.read()
     divisao = 10 / 1
     numero = lista[1]
-    x = a 
+    print('Fechando o arquivo!')
 except ZeroDivisionError:
-    print('Não é possível realizar uma divisão por 0!')
+    print('Não é possível realizar divisão por zero!')   
+except ArithmeticError:
+    print('Houve um erro ao realizar uma operação aritmética')
 except IndexError:
     print('Erro ao acessar um indice da lista')
 except BaseException as ex: #O BaseException é o pai das exceções. 
     print('Erro desconhecido. Erro: {}' .format(ex))
+else:
+    print('Executa quando não ocorre exceção!')
+finally:
+    print('Sempre executa!')
+    arquivo.close()
+
+
